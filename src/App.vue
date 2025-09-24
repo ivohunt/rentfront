36af5f50ea1a4967e1bc1c4d63fc1d914bd85dcd
ivohunt <ivo.hunt@gmail.com>
2025-09-24 09:26:34 +0300

    Kasutaja lisamine töötab

--- src/App.vue
+++ src/App.vue
@@ -15,7 +15,11 @@
 
 
     <template v-if="isLoggedIn">
+      <router-link to="/price-edit">Hinnad & kategooriad</router-link>
+      |
       <font-awesome-icon @click="startLogout" type="button" icon="fa-solid fa-right-from-bracket" />
+
+
     </template>
 
 
