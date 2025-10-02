8c5a99cbac3425e6c0b6ba251306ae4d61d3b944
Slk-hub <netslauk@gmail.com>
2025-10-02 12:35:39 +0300

    imagemissing suusa asemel

--- src/components/image/ItemAddImg.vue
+++ src/components/image/ItemAddImg.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="mt-5 mb-2">
-    <img v-if="imageData === '' || imageData === null || imageData === undefined"  src="../../assets/suusk.png" class="img-thumbnail" alt="Varustuse pilt">
+    <img v-if="imageData === '' || imageData === null || imageData === undefined"  src="../../assets/imagemissing.png" class="img-thumbnail" alt="Varustuse pilt">
     <img v-else :src="imageData" class="img-thumbnail" alt="Varustuse pilt">
   </div>
 </template>
